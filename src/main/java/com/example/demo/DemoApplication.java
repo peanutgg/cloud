bc53447ae0a2fefbbc59e536e9a0565c2233aaa0
peanutgg <694507678@qq,com>
2020-06-12 10:31:56 +0800

    2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import com.example.demo.Conf.Conf;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@Slf4j
 public class DemoApplication {
-
+    @Autowired
+    private Conf conf;
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
     @GetMapping("/test")
     public String test(){
+
+        log.info(conf.getName());
+
         return "hello world";
     }
 }
