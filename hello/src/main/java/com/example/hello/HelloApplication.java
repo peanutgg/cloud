c7a06fa461ed7a2264818f1f8c7cf8636b3f82b7
peanutgg <694507678@qq,com>
2020-06-15 17:47:00 +0800

    44

--- hello/src/main/java/com/example/hello/HelloApplication.java
+++ hello/src/main/java/com/example/hello/HelloApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
-//@RibbonClient(name="hello-client-service")
+@RibbonClient(name="hello-client-service")
 public class HelloApplication {
 
     public static void main(String[] args) {
