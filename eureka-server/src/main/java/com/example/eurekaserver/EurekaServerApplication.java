bd2d3653721977884e4b36804472b36b83bba985
peanutgg <694507678@qq,com>
2020-06-15 13:41:45 +0800

    44

--- eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -4,15 +4,24 @@ import com.example.demo.Filter.InportAutoConfiguration.annotations.EnableLogFilt
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
 @EnableLogFilter
+@RestController
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
 
         SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+
+        return "hello eureka sever";
     }
 
 }
