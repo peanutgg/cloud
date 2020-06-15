ad8e54adb36200d6cb5fda7b7c31940d6b8be601
peanutgg <694507678@qq,com>
2020-06-15 17:36:26 +0800

    44

--- query/src/main/java/com/example/query/QueryApplication.java
+++ query/src/main/java/com/example/query/QueryApplication.java
@@ -1,24 +1,48 @@
 package com.example.query;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class QueryApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(QueryApplication.class, args);
     }
 
-    @RequestMapping("/test")
+    @Value("${spring.application.name}")
+    private String name;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/askHello")
     public String test() {
 
-        return "hello eureka client";
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://HELLO-CLIENT-SERVICE/hello/{name}", String.class, name);
+        String askHelloService = forEntity.getBody();
+        return askHelloService;
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+
+        return new RestTemplate();
+
     }
 
 }
