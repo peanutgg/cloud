a964402d05a86cc1f629cdb53ac2ea4c4de89963
peanutgg <35030907+peanutgg@users.noreply.github.com>
2020-06-18 00:53:22 +0800

    hystrix complete 50% go ahead

--- hystrix/src/main/java/com/example/hystrix/HystrixApplication.java
+++ hystrix/src/main/java/com/example/hystrix/HystrixApplication.java
@@ -4,12 +4,14 @@ package com.example.hystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableDiscoveryClient
 public class HystrixApplication {
 
     public static void main(String[] args) {
