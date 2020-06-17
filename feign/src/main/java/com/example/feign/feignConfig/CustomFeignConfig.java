624754c9d5f7eb3dbbf53b16c67d1f4a93d91ba0
peanutgg <694507678@qq,com>
2020-06-17 15:01:57 +0800

    44

---
+++ feign/src/main/java/com/example/feign/feignConfig/CustomFeignConfig.java
@@ -0,0 +1,46 @@
+package com.example.feign.feignConfig;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
+
+
+public class CustomFeignConfig {
+    @Bean
+    public Decoder feignDecoder() {
+
+        HttpMessageConverter jacksonConverter = new MappingJackson2CborHttpMessageConverter(customObjectMapper());
+        ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(jacksonConverter);
+        return new ResponseEntityDecoder(new SpringDecoder(objectFactory));
+    }
+
+    @Bean
+
+    public Encoder feignEncoder() {
+
+        HttpMessageConverter jacksonConverter = new MappingJackson2CborHttpMessageConverter(customObjectMapper());
+        ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(jacksonConverter);
+        return new SpringEncoder(objectFactory);
+    }
+
+
+    public ObjectMapper customObjectMapper() {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+
+        return objectMapper;
+    }
+
+
+}
