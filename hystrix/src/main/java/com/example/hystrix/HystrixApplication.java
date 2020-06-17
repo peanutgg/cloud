ffcba895782ac2f1f6c6ee6ef033fd5560c740f8
peanutgg <35030907+peanutgg@users.noreply.github.com>
2020-06-17 22:09:00 +0800

    213123

--- hystrix/src/main/java/com/example/hystrix/HystrixApplication.java
+++ hystrix/src/main/java/com/example/hystrix/HystrixApplication.java
@@ -1,13 +1,25 @@
 package com.example.hystrix;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class HystrixApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HystrixApplication.class, args);
     }
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
 
 }
