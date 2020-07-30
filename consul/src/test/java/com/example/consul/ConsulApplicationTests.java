b4098277fb65212fb9663abb3afbacde86b04be0
peanutgg <35030907+peanutgg@users.noreply.github.com>
2020-07-30 21:42:28 +0800

    第一天

--- consul/src/test/java/com/example/consul/ConsulApplicationTests.java
+++ consul/src/test/java/com/example/consul/ConsulApplicationTests.java
@@ -1,12 +1,12 @@
 package com.example.consul;
 
 import org.junit.jupiter.api.Test;
-
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
 @SpringBootTest
+//@SpringBootConfiguration
 public class ConsulApplicationTests {
 
     @Test
